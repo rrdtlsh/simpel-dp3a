49f844dbe8ac5f4100416e2f04da7d96225c6204
aliyarfnaura <2310817120014@mhs.ulm.ac.id>
2026-04-16 11:17:45 +0800

    16/04/2026 11.17

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->foreignId('bidang_id')->nullable();
+            //$table->foreignId('bidang_id')->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
